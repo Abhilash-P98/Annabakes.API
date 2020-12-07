b84bf1735f6b65d8942bcecd9a4bba9548df807f
abhil <abhil@LAPTOP-13NC4DCI>
2020-12-07 16:24:25 +0530

    updated

--- Migrations/20201207104313_User.Designer.cs
+++ Migrations/20201207104313_User.Designer.cs
@@ -2,15 +2,17 @@
 using Annabakes.API.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Annabakes.API.Migrations
 {
     [DbContext(typeof(BakeryDbContext))]
-    partial class BakeryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201207104313_User")]
+    partial class User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
